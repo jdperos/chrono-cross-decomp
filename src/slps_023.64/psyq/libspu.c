4ced8a75460548bfb25e0a1c4360bb51dcacc13a
Jonathan Peros <jperos@jenova.games>
2025-09-19 13:55:38 -0700

    Identifying libspu

---
+++ src/slps_023.64/psyq/libspu.c
@@ -0,0 +1,43 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", SpuInit);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _SpuInit);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", SpuStart);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_init);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_FwriteByIO);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_FiDMA);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_Fr_);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_t);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_Fw);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_Fr);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_FsetRXX);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_FsetRXXa);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_getRXXa);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_FsetPCR);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_FsetDelayW);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_FsetDelayR);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _spu_Fw1ts);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _SpuDataCallback);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", SpuSetReverb);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _SpuIsInAllocateArea);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu", _SpuIsInAllocateArea_);
